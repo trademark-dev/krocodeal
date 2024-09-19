b3a1fdeadef11b96a356e32576e0f32d721e0120
trademark-dev <alixjutt34@gmail.com>
2024-09-19 02:52:15 -0700

    last

---
+++ vender/plugin/appfour.js
@@ -0,0 +1,43 @@
+(function($) {
+    "use strict";
+
+    function rt_countdown() {
+        if ($('.count-down-style-1').length) {
+            $('.count-down-style-1').each(function() {
+                var $CountdownSelector = $(this).find('.countdown');
+                var eventCountdownTime = $(this).data('time');
+                $CountdownSelector.countdown(eventCountdownTime).on('update.countdown', function(event) {
+                    var totalHours = event.offset.totalHours;
+                    var totalMinutes = event.offset.totalMinutes;
+                    var totalSeconds = event.offset.totalSeconds;
+
+                    // Days Section
+                    var daysHtml = (totalHours > 23) ?
+                        '<div class="count-down-block"><span>%D</span> <div class="inner_rtyth">D</div></div>' :
+                        '';
+                    // Hours Section
+                    var hoursHtml = (totalMinutes > 59) ?
+                        '<div class="count-down-block"><span>%H</span> <div class="inner_rtyth">H</div></div>' :
+                        '';
+                    // Minutes Section
+                    var minutesHtml = (totalSeconds > 59) ?
+                        '<div class="count-down-block"><span>%M</span><div class="inner_rtyth"> M</div></div>' :
+                        '';
+                    // Seconds Section (Always Shown)
+                    var secondsHtml = '<div class="count-down-block"><span>%S</span> <div class="inner_rtyth">S</div></div>';
+
+                    $(this).html(
+                        event.strftime(daysHtml + hoursHtml + minutesHtml + secondsHtml)
+                    );
+                }).on('finish.countdown', function(event) {
+                    $(this).html(event.strftime(''));
+                });
+            });
+        }
+    }
+
+    $(window).on('load', function() {
+        rt_countdown();
+    });
+
+})(jQuery);
